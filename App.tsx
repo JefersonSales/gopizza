27faaffd700f36d4de1118cb2f810d5da0266a8d
Jeferson Sales <jefersonsales1@gmail.com>
2022-01-29 11:14:09 -0300

    commit inicial

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import AppLoading from 'expo-app-loading'
+import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+import { ThemeProvider } from 'styled-components/native'
+
+import theme from './src/theme';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular
+  })
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+  return (
+    <ThemeProvider theme={theme}>
+    </ThemeProvider>
+  );
+}
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ts',
+          '.tsx',
+          '.js',
+          '.json'
+        ],
+        alias: {
+          '@components': '.src/components',
+          '@assets': '.src/assets',
+          '@screens': '.src/screens',
+      }
+      }
+    ]
   };
 };
